4223bcbc4c1e00a9632515c51d4d04d70b4a7d45
Melike Aldemir <melikealdmir@gmail.com>
2025-03-06 12:40:09 +0300

    week3-melikealdmir

--- week3/src/FindMaximun.java
+++ week3/src/FindMaximun.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class FindMaximun {
+    public static void main (String[] args){
+        int num1=Integer.parseInt(args[0]);
+        int num2=Integer.parseInt(args[1]);
+
+        boolean condition = num1>num2;
+
+        int greatest =condition ? num1:num2;
+        System.out.println(greatest);
+
+        System.out.println("En büyük sayı: " + greatest);
+
+    }
 }
